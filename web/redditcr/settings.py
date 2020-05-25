d677adfcea4c99761cc3a25faf10c900636c414a
Vyacheslav Kurai <vyacheslav.kurai@gmail.com>
2020-05-25 16:32:26 +0300

    Fix docker-compose and settings

--- web/redditcr/settings.py
+++ web/redditcr/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'myn4gqf3qk1y2^o^t=uhu2$ecx&ib5d6!cp%d@yasczlzcv9^)'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
